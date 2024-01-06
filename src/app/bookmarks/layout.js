0507a86eb9439af68f1e9a55e472e7024fd24c18
Haris <harisxstudy@gmail.com>
2024-01-07 01:03:48 +0500

    changed metadata location.

---
+++ src/app/bookmarks/layout.js
@@ -0,0 +1,9 @@
+import Page from "./page"; // import your Demo's page
+
+export const metadata = {
+  title: "Bookmarked Posts - Posts Manager",
+  description:
+    "Explore and manage your bookmarked posts effortlessly with the Posts Manager. Unbookmark posts, view details, and navigate seamlessly through your saved content. Your central hub for organized and accessible bookmarked posts.",
+};
+
+export default Page;
--- src/app/bookmarks/page.js
+++ src/app/bookmarks/page.js
@@ -6,11 +6,6 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { ArrowLeft, Bookmark, Trash } from "react-feather";
 
-export const metadata = {
-  title: "Bookmarked Posts - Posts Manager",
-  description:
-    "Explore and manage your bookmarked posts effortlessly with the Posts Manager. Unbookmark posts, view details, and navigate seamlessly through your saved content. Your central hub for organized and accessible bookmarked posts.",
-};
 
 function Bookmarks() {
   const [data, setData] = useState([]);
